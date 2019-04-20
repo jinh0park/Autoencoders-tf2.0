0baf8c85103029cd983ba18b2678838527e6325f
jinh0park <jinh0park@naver.com>
2019-04-20 16:35:00 +0900

    train model code

---
+++ train_utils/autoencoder.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+
+
+class AETrain:
+    @staticmethod
+    def compute_loss(model, x):
+        loss_object = tf.keras.losses.BinaryCrossentropy()
+        loss = loss_object(x, model(x))
+        return loss
+
+    @staticmethod
+    def compute_gradients(model, x):
+        with tf.GradientTape() as tape:
+            loss = AETrain.compute_loss(model, x)
+        return tape.gradient(loss, model.trainable_variables), loss
+
+    @staticmethod
+    def apply_gradients(optimizer, gradients, variables):
+        optimizer.apply_gradients(zip(gradients, variables))
+
+
+class VAETrain:
+    @staticmethod
+    def compute_loss(model, x):
+        mean, logvar = model.encode(x)
+        z = model.reparameterize(mean, logvar)
+        x_logits = model.decode(z)
+
+        # cross_ent = - marginal likelihood
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logits, labels=x)
+        marginal_likelihood = - tf.reduce_sum(cross_ent, axis=1)
+        marginal_likelihood = tf.reduce_mean(marginal_likelihood)
+
+        KL_divergence = tf.reduce_sum(mean ** 2 + tf.exp(logvar) - logvar - 1, axis=1)
+        KL_divergence = tf.reduce_mean(KL_divergence)
+
+        ELBO = marginal_likelihood - KL_divergence
+        loss = -ELBO
+        return loss
+
+    @staticmethod
+    def compute_gradients(model, x):
+        with tf.GradientTape() as tape:
+            loss = VAETrain.compute_loss(model, x)
+        return tape.gradient(loss, model.trainable_variables), loss
+
+    @staticmethod
+    def apply_gradients(optimizer, gradients, variables):
+        optimizer.apply_gradients(zip(gradients, variables))
+
+
+class CVAETrain:
+    @staticmethod
+    def compute_loss(model, x, y):
+        mean, logvar = model.encode(x, y)
+        z = model.reparameterize(mean, logvar)
+        x_logits = model.decode(z, y)
+
+        # cross_ent = - marginal likelihood
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logits, labels=x)
+        marginal_likelihood = - tf.reduce_sum(cross_ent, axis=1)
+        marginal_likelihood = tf.reduce_mean(marginal_likelihood)
+
+        KL_divergence = tf.reduce_sum(mean ** 2 + tf.exp(logvar) - logvar - 1, axis=1)
+        KL_divergence = tf.reduce_mean(KL_divergence)
+
+        ELBO = marginal_likelihood - KL_divergence
+        loss = -ELBO
+        return loss
+
+    @staticmethod
+    def compute_gradients(model, x, y):
+        with tf.GradientTape() as tape:
+            loss = CVAETrain.compute_loss(model, x, y)
+        return tape.gradient(loss, model.trainable_variables), loss
+
+    @staticmethod
+    def apply_gradients(optimizer, gradients, variables):
+        optimizer.apply_gradients(zip(gradients, variables))
